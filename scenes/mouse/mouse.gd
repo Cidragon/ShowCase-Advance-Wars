225b66b4b83fa0a70d5e06e6d5ad2bad769b2b26
Cidragon <darioperezpastrian@gmail.com>
2024-03-13 15:59:59 -0300

    hide walkable tiles on deselect unit

--- scenes/mouse/mouse.gd
+++ scenes/mouse/mouse.gd
@@ -3,9 +3,12 @@ extends Node2D
 @onready var cursor: Sprite2D = %cursor
 
 var is_unit_selected : bool = false
+var unit_selected : Vector2i
+const DEFAULT_NO_UNIT : Vector2i = Vector2i(-1,-1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	unit_selected = DEFAULT_NO_UNIT
 	pass # Replace with function body.
 
 
@@ -24,10 +27,16 @@ func _unhandled_input(event: InputEvent) -> void:
 		#print(unit)
 		if unit:
 			is_unit_selected = true
+			unit_selected = grid_position
 			print("there is a unit")
 			unit.show_walkable_tiles()
 	elif event.is_action_pressed("left_click") and is_unit_selected == false:
 		print("move character")
 		pass
 	elif event.is_action_pressed("right_click"):
+		if unit_selected != DEFAULT_NO_UNIT:
+			var unit : Unit = get_parent().units.get(unit_selected)
+			unit.hide_walkable_tiles()
+			unit_selected = DEFAULT_NO_UNIT
+			
 		is_unit_selected = false
