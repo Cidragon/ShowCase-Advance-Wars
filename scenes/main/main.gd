33ca4e55d50da1b16c4c5235b63fe38ccd69cf5f
Cidragon <darioperezpastrian@gmail.com>
2024-03-13 19:13:29 -0300

    walkable area boolean checker

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,24 +4,27 @@ class_name Main
 @export var unit_scene : PackedScene
 
 const TILE_SIZE : Vector2 = Vector2(16,16)
-
+const MAP_SIZE : Vector2 = Vector2(51,34)
 
 
 var units: Dictionary = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	create_soldier()
+	create_soldier(Vector2(16,8))
+	create_soldier(Vector2(24,12))
+	
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func create_soldier() -> void:
+func create_soldier(grid_position : Vector2 = Vector2(-1,-1)) -> void:
 	var soldier : Unit = unit_scene.instantiate()
-	soldier.position = Vector2(320, 256)
+	soldier.position = Vector2(320, 256) #default value
+	if grid_position != Vector2(-1,-1):
+		soldier.position = grid_position * TILE_SIZE
 	add_child(soldier)
 	units[soldier.grid_position] = soldier
 	print("soldier grid position: ", soldier.grid_position)
--- scenes/mouse/mouse.gd
+++ scenes/mouse/mouse.gd
@@ -19,9 +19,29 @@ func _process(delta: float) -> void:
 		
 		position = Vector2(grid_position) * Main.TILE_SIZE
 
+func is_inside_map(grid_position : Vector2i) -> bool:
+	var x : int = grid_position.x
+	var y : int = grid_position.y
+	if x >= Main.MAP_SIZE.x or x < 0 or y >= Main.MAP_SIZE.y or y < 0:
+		return false
+	return true
+
+func is_inside_walkable_area(grid_position: Vector2i) -> bool:
+	if is_inside_map(grid_position):
+		var unit : Unit = get_parent().units.get(unit_selected)
+		if not unit:
+			return false
+		
+		var distance_to_unit = unit.get_manhattan_distance(unit.grid_position, grid_position)
+		if distance_to_unit <= unit.move_distance:
+			return true
+	
+	return false
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click") and is_unit_selected == false:
 		var grid_position : Vector2i = Vector2i(get_global_mouse_position() / Main.TILE_SIZE)
+		print("is inside map? :" , is_inside_map(grid_position))
 		#print(grid_position)
 		var unit : Unit = get_parent().units.get(grid_position)
 		#print(unit)
@@ -30,8 +50,10 @@ func _unhandled_input(event: InputEvent) -> void:
 			unit_selected = grid_position
 			print("there is a unit")
 			unit.show_walkable_tiles()
-	elif event.is_action_pressed("left_click") and is_unit_selected == false:
+	elif event.is_action_pressed("left_click") and is_unit_selected == true:
 		print("move character")
+		var grid_position : Vector2i = Vector2i(get_global_mouse_position() / Main.TILE_SIZE)
+		print("is position walkable: " , is_inside_walkable_area(grid_position))
 		pass
 	elif event.is_action_pressed("right_click"):
 		if unit_selected != DEFAULT_NO_UNIT:
